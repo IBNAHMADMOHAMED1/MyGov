008e92f549a479d39cf91895959ef943dcb8cb8c
IBNAHMADMOHAMED1 <ibnahamdmohamed8@gmail.com>
2023-02-02 09:59:35 +0000

    first commit

--- src/main/java/ma/maroc/mygov/MyGovApplication.java
+++ src/main/java/ma/maroc/mygov/MyGovApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MyGovApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MyGovApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MyGovApplication.class, args);
+    }
 
 }
--- src/test/java/ma/maroc/mygov/MyGovApplicationTests.java
+++ src/test/java/ma/maroc/mygov/MyGovApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class MyGovApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
