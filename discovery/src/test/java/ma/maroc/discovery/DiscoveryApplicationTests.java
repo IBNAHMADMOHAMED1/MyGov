12cf8eddfcdaa9083e2ac27a113779874d7b7c4a
IBNAHMADMOHAMED1 <ibnahamdmohamed8@gmail.com>
2023-02-02 10:22:04 +0000

    Discovery

---
+++ discovery/src/test/java/ma/maroc/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.maroc.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
