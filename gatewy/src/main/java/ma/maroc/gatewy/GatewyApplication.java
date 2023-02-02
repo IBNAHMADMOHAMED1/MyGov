04267a0267b88fdcb0de072f2fe42ae234f62cfe
IBNAHMADMOHAMED1 <ibnahamdmohamed8@gmail.com>
2023-02-02 10:22:21 +0000

    Add gatewy

---
+++ gatewy/src/main/java/ma/maroc/gatewy/GatewyApplication.java
@@ -0,0 +1,13 @@
+package ma.maroc.gatewy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewyApplication.class, args);
+    }
+
+}
---
+++ gatewy/src/test/java/ma/maroc/gatewy/GatewyApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.maroc.gatewy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
