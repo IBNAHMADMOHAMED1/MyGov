eec6526a4f934162a12fd77034181d7c6b1a24fa
IBNAHMADMOHAMED1 <ibnahamdmohamed8@gmail.com>
2023-02-02 10:07:10 +0000

    Add Auth Service

---
+++ auth-service/src/test/java/ma/maroc/authservice/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.maroc.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
