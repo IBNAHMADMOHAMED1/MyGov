c73456625e326b1574ac061f74c1eebb3b6730c5
IBNAHMADMOHAMED1 <ibnahamdmohamed8@gmail.com>
2023-02-02 10:14:51 +0000

    Config services

---
+++ config/src/test/java/ma/maroc/config/ConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.maroc.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
